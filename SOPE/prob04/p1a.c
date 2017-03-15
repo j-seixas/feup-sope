00d4f62812a45f897c9c7733a1494e63a07238e9
João Seixas <myownxdeath@users.noreply.github.com>
2017-03-15 12:22:03 +0000

    Add files via upload

---
+++ SOPE/prob04/p1a.c
@@ -0,0 +1,33 @@
+// PROGRAMA p01a.c
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void sigint_handler(int signo)
+{
+  printf("In SIGINT handler ...\n");
+}
+
+
+
+int main(void){
+
+  struct sigaction action;
+  action.sa_handler = sigint_handler;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+
+
+  if (sigaction(SIGINT, &action, NULL) < 0)
+  {
+    fprintf(stderr,"Unable to install SIGINT handler\n");
+    exit(1);
+  }
+  printf("Sleeping for 30 seconds ...\n");
+  int time = sleep(30);
+  while(time > 0)
+    time = sleep(time);
+  printf("Waking up ...\n");
+  exit(0);
+}
