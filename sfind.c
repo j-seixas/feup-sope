5f32e2ad3b61eb0cca7b8b46c5f3c40a96630185
rendoir <up201503822@fe.up.pt>
2017-04-11 17:46:11 +0100

    Added -perm

--- sfind.c
+++ sfind.c
@@ -9,21 +9,45 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <ctype.h>
 
 
 typedef struct{
-  char *name;
-  int toPrint, toDelete, hasName, hasType, hasPerm, hasExec;
+  char *name, *perm_mode;
+  char toPrint, toDelete, hasName, hasType, hasPerm, hasExec;
 } Flags;
 
-void initFlags(Flags *flag){
-	flag->name  = malloc(sizeof(char *));
-	flag->toPrint = 0;
-	flag->toDelete = 0;
-	flag->hasName = 0;
-	flag->hasType = 0;
-	flag->hasPerm = 0;
-	flag->hasExec = 0;
+void initFlags(Flags *flags){
+  flags->name  = malloc(256 * sizeof(char));
+  flags->perm_mode = malloc(3 * sizeof(char));
+	flags->toPrint = 0;
+	flags->toDelete = 0;
+	flags->hasName = 0;
+	flags->hasType = 0;
+	flags->hasPerm = 0;
+	flags->hasExec = 0;
+}
+
+char validArguments(Flags *flags){
+    return !(
+        ((!strcmp(flags->name, "")) && flags->hasName) ||
+        ((!strcmp(flags->name, "")) && flags->hasPerm) ||
+        ((!strcmp(flags->perm_mode, "")) && flags->hasPerm) ||
+        ((!strcmp(flags->name, "")) && flags->toDelete)
+      );
+}
+
+char isNumber(const char* str) {
+  if(str == NULL)
+    return 0;
+  int i = 0;
+  char ch;
+  while( (ch = str[i]) != '\0') {
+    if(!isdigit(ch))
+      return 0;
+    i++;
+  }
+  return 1;
 }
 
 int main(int argc, char *argv[])
@@ -36,20 +60,29 @@ int main(int argc, char *argv[])
   Flags flags;
 	initFlags(&flags);
 
-  int args = 3;
-  while (args <= argc){
-    if(!strcmp(argv[args-1] , "-print"))
+  int args = 2;
+  while (args < argc){
+    if(!strcmp(argv[args] , "-print"))
       flags.toPrint = 1;
-    else if(!strcmp(argv[args-1] , "-delete"))
+    else if(!strcmp(argv[args] , "-delete"))
       flags.toDelete = 1;
-    else if(!strcmp(argv[args-1] , "-name") && args+1 <= argc){
+    else if(!strcmp(argv[args] , "-name"))
       flags.hasName = 1;
-			strcpy(flags.name,argv[args]);
-		//	printf("%d - %s\n", hasName, name);
-			args++;
-    }
-	//	printf("%d Name - %d Delete - %d Print\n", hasName, toDelete, toPrint);
+    else if(!strcmp(argv[args] , "-perm"))
+      flags.hasPerm = 1;
+    else if(isNumber(argv[args]))
+      strcpy(flags.perm_mode, argv[args]);
+    else
+      strcpy(flags.name, argv[args]);
     args++;
+  }
+
+  //printf("Name: |%s|\n", flags.name);
+  //printf("Perms: |%s|\n", flags.perm_mode);
+
+  if(!validArguments(&flags)){
+    printf("Invalid Arguments\n");
+    return 1;
   }
 
 	DIR *curr_dir;
@@ -87,8 +120,6 @@ int main(int argc, char *argv[])
         return 1;
       }
 
-			/*else
-				waitpid(pid,NULL,0);*/
 
 		}
 	}
@@ -96,11 +127,10 @@ int main(int argc, char *argv[])
   rewinddir(curr_dir);
   while ( (dir_info = readdir(curr_dir) ) != NULL){
 
-		if(flags.hasName){
-			//printf("HAS NAME\n");
+      //printf("File |%s|\n", dir_info->d_name);
 
 			if(!strcmp(flags.name, dir_info->d_name) && flags.toPrint)
-				printf("%d - %s/%s   <---- FOUND\n",getpid() ,  argv[1], dir_info->d_name );
+				printf("\n    ----> %d - %s/%s\n\n", getpid() , argv[1], dir_info->d_name );
 			else if(!strcmp(flags.name, dir_info->d_name) && !flags.toPrint)
 				printf("%d - %s/%s\n",getpid() ,  argv[1], dir_info->d_name );
 			else if(flags.toPrint)
@@ -112,16 +142,32 @@ int main(int argc, char *argv[])
 					int size = 1 + snprintf(NULL, 0, "%s/%s", argv[1], dir_info->d_name);
         	argv_new[1] = malloc(size);
         	snprintf(argv_new[1], size, "%s/%s", argv[1], dir_info->d_name);
-					execlp("rm", "-i", argv_new[1], NULL);
+					execlp("rm", "-i", "-r", "-f", argv_new[1], NULL);
 					free(argv_new[1]);
         	free(argv_new);
         	printf("Error in process %d\n", getpid());
 					return 1;
 				}
 			}
-		} else if (flags.toPrint)
-				printf("%d - %s/%s\n",getpid() ,  argv[1], dir_info->d_name );
-  }
+
+
+      if(flags.hasPerm && !strcmp(flags.name, dir_info->d_name)){
+        //printf("Changing perms of file |%s|\n", dir_info->d_name);
+
+        if( (pid = fork()) == 0){
+          int size = 1 + snprintf(NULL, 0, "%s/%s", argv[1], dir_info->d_name);
+          argv_new[1] = malloc(size);
+          snprintf(argv_new[1], size, "%s/%s", argv[1], dir_info->d_name);
+          execlp("chmod", "-f", flags.perm_mode, argv_new[1], NULL);
+          free(argv_new[1]);
+          free(argv_new);
+          printf("Error in process %d\n", getpid());
+          return 1;
+        }
+      }
+
+
+		}
 
 	return 0;
 
