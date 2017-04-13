85fc1c35914ccbc4793880d0c156a1da199edd0a
rendoir <up201503822@fe.up.pt>
2017-04-13 15:46:33 +0100

    Re-structured the whole program, works fine, but still needs revision of the prints

--- sfind.c
+++ sfind.c
@@ -14,28 +14,31 @@
 
 typedef struct{
   char *name, *perm_mode;
-  char toPrint, toDelete, hasName, hasType, hasPerm, hasExec;
+  char toPrint, toDelete, hasName, hasType, hasPerm, hasExec, type;
+  int perm;
 } Flags;
 
 void initFlags(Flags *flags){
-  flags->name = malloc(1024 * sizeof(char));
+  flags->name = malloc(NAME_MAX);
   strcpy(flags->name, "");
-  flags->perm_mode = malloc(32 * sizeof(char));
+  flags->perm_mode = malloc(16);
   strcpy(flags->perm_mode, "");
+  flags->type = '\0';
   flags->toPrint = 0;
 	flags->toDelete = 0;
 	flags->hasName = 0;
 	flags->hasType = 0;
 	flags->hasPerm = 0;
 	flags->hasExec = 0;
+  flags->perm = -1;
 }
 
 char validArguments(Flags *flags){
     return !(
         ((!strcmp(flags->name, "")) && flags->hasName) ||
-        ((!strcmp(flags->name, "")) && flags->hasPerm) ||
         ((!strcmp(flags->perm_mode, "")) && flags->hasPerm) ||
-        ((!strcmp(flags->name, "")) && flags->toDelete)
+        ((!strcmp(flags->name, "")) && flags->toDelete) ||
+        (flags->type == '\0' && flags->hasType)
       );
 }
 
@@ -57,7 +60,7 @@ void sigint_handler(int sign){
   do{
       printf("\n\n Are you sure you want to terminate? ");
       scanf("%c", &c);
-  
+
       if(c == 'y' || c == 'Y')
         exit(0);
       else if(c == 'n' || c == 'N')
@@ -67,7 +70,7 @@ void sigint_handler(int sign){
 
   }while( 1 );
 }
-  
+
 
 int main(int argc, char *argv[])
 {
@@ -92,15 +95,24 @@ int main(int argc, char *argv[])
       args++;
     }else if(!strcmp(argv[args] , "-perm") && args+1 < argc){
       flags.hasPerm = 1;
-      if(isNumber(argv[args+1]))
+      if(isNumber(argv[args+1])){
         strcpy(flags.perm_mode, argv[args+1]);
+        flags.perm = strtol(flags.perm_mode, NULL, 8);
+      }
+      args++;
+    }else if(!strcmp(argv[args] , "-type") && args+1 < argc){
+      flags.hasType = 1;
+      flags.type = argv[args+1][0];
       args++;
     }
     args++;
   }
 
+  //Debug
   //printf("Name: |%s|\n", flags.name);
   //printf("Perms: |%s|\n", flags.perm_mode);
+  //printf("Type: |%c|\n", flags.type);
+  //printf("Perm args: |%s||%d|\n", flags.perm_mode, flags.perm);
 
   if(!validArguments(&flags)){
     printf("Invalid Arguments\n");
@@ -111,10 +123,10 @@ int main(int argc, char *argv[])
 	struct dirent *dir_info;
 	int pid = 0;
 
-  char *directory = malloc(1024 * sizeof(char));
+  char *directory = malloc(PATH_MAX);
   strcpy(directory,argv[1]);
 	strcat(directory,"/");
-	//printf("Trying to open dir %s\n",directory);
+
 	if ( (curr_dir = opendir(directory))  == NULL){
 		perror("Error 1: ");
 		exit(1);
@@ -130,75 +142,64 @@ int main(int argc, char *argv[])
     exit(2);
   }
 
-  char **argv_new;
   size_t a_size = sizeof(char*) * (argc + 1);
-  argv_new = malloc(a_size);
+  char **argv_new = malloc(a_size);
   memcpy(argv_new, argv, a_size);
 
-
-	while ( (dir_info = readdir(curr_dir) ) != NULL){
-		//printf("%d - %s\n",getpid() ,  dir_info->d_name );
-		if ( dir_info->d_type == DT_DIR && (strcmp(dir_info->d_name,".") != 0 && (strcmp(dir_info->d_name,"..") != 0))){
+  while ( (dir_info = readdir(curr_dir) ) != NULL){
+		if ( dir_info->d_type == DT_DIR && strcmp(dir_info->d_name,".") && strcmp(dir_info->d_name,"..")){
 			if( (pid = fork()) == 0){
-        int size = 1 + snprintf(NULL, 0, "%s/%s", argv[1], dir_info->d_name);
-        argv_new[1] = malloc(size);
-        snprintf(argv_new[1], size, "%s/%s", argv[1], dir_info->d_name);
-
+        //Calculate new path name
+        argv_new[1] = malloc(PATH_MAX);
+        strcpy(argv_new[1], argv[1]);
+        strcat(argv_new[1], "/");
+        strcat(argv_new[1], dir_info->d_name);
+        //Execute this program with the new path
         execv(argv_new[0], argv_new);
         perror("Error 3: ");
         exit(3);
-      } 
+      }
       else {
-        wait(NULL);
-        //free(argv_new[1]);
-        //free(argv_new);
+        wait(NULL);;
       }
 		}
 	}
 
   rewinddir(curr_dir);
   while ( (dir_info = readdir(curr_dir) ) != NULL){
+      //Calculate full path name
+      char *full_name = malloc(PATH_MAX);
+      strcpy(full_name, argv[1]);
+      strcat(full_name, "/");
+      strcat(full_name, dir_info->d_name);
 
-      //printf("File |%s|\n", dir_info->d_name);
+      //Calculate permissions
+      struct stat buf;
+      stat(full_name, &buf);
+      int mode = buf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
 
-			if(!strcmp(flags.name, dir_info->d_name) && flags.toPrint)
-				printf("\n    ----> %d - %s/%s\n\n", getpid() , argv[1], dir_info->d_name );
-			else if(!strcmp(flags.name, dir_info->d_name) && !flags.toPrint)
-				printf("%d - %s/%s\n",getpid() ,  argv[1], dir_info->d_name );
-			else if(flags.toPrint)
-				printf("%d - %s/%s\n",getpid() ,  argv[1], dir_info->d_name );
+      //Check if the current file/directory has the same properties as the arguments
+      char same_name = !strcmp(flags.name, dir_info->d_name);
+      char same_perm = flags.perm == mode;
+      char same_type = ((dir_info->d_type == DT_REG) && (flags.type == 'f')) ||
+                       ((dir_info->d_type == DT_DIR) && (flags.type == 'd')) ||
+                       ((dir_info->d_type == DT_LNK) && (flags.type == 'l'));
 
+      //Check if the current file/directory should be printed
+      if((flags.hasName && same_name) ||
+         (flags.hasPerm && same_perm) ||
+         (flags.hasType && same_type)) {
+            printf("\n%s\n", full_name);
+      }
 
-			if(flags.toDelete && !strcmp(flags.name, dir_info->d_name)){
+      //Check if the current file/directory should be deleted
+			if(flags.toDelete && same_name){
 				if( (pid = fork()) == 0){
-					int size = 1 + snprintf(NULL, 0, "%s/%s", argv[1], dir_info->d_name);
-        	argv_new[1] = malloc(size);
-        	snprintf(argv_new[1], size, "%s/%s", argv[1], dir_info->d_name);
-					execlp("rm", "-i", "-r", "-f", argv_new[1], NULL);
-					free(argv_new[1]);
-        	free(argv_new);
-        	printf("Error in process %d\n", getpid());
+					execlp("rm", "-i", "-r", "-f", full_name, NULL);
+        	printf("Error in process %d - Deleting file %s\n", getpid(), full_name);
 					return 1;
 				}
 			}
-
-
-      if(flags.hasPerm && !strcmp(flags.name, dir_info->d_name)){
-        //printf("Changing perms of file |%s|\n", dir_info->d_name);
-
-        if( (pid = fork()) == 0){
-          int size = 1 + snprintf(NULL, 0, "%s/%s", argv[1], dir_info->d_name);
-          argv_new[1] = malloc(size);
-          snprintf(argv_new[1], size, "%s/%s", argv[1], dir_info->d_name);
-          execlp("chmod", "-f", flags.perm_mode, argv_new[1], NULL);
-          free(argv_new[1]);
-          free(argv_new);
-          printf("Error in process %d\n", getpid());
-          return 1;
-        }
-      }
-
-
 		}
 
 	return 0;
